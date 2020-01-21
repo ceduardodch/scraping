8d7e73af250037482943c78f06f6e1451ed6a5b4
Jefferson <jeffer_tambaco@hotmail.es>
2020-01-21 13:05:46 -0500

    SqlLite

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,7 +1,8 @@
-import React, {Component} from "react";
-import {StyleSheet,View, Text,SafeAreaView, ScrollView } from  "react-native"
+import React, { Component } from "react";
+import { StyleSheet, View, Text, SafeAreaView, ScrollView,Alert } from "react-native"
 import Odoo from 'react-native-odoo-promise-based'
-import { Button, Input, Card  } from "react-native-elements";
+import * as SQLite from 'expo-sqlite';
+import { Button, Input, Card } from "react-native-elements";
 import PreLoader from "../components/PreLoader"
 import { FacturaStruct, FacturaOptions } from "../forms/Factura";
 import t from "tcomb-form-native";
@@ -9,180 +10,220 @@ import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-ta
 
 
 const Form = t.form.Form;
-export default class Home extends Component
-{
-    constructor(props) {
-        super(props)
-        this.state = {
+const db = SQLite.openDatabase("Factura.db");
+
+export default class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
 
 
-          facturaStruct: FacturaStruct,
-          facturaOptions: FacturaOptions,
-          facturaData: {
-            cedula: "",
-            cantidad: "",
-            monto: ""
-          },
-          facturaErrorMessage: "",
-      
-          formRegistro:{
-          names: '',
-          lastnames: '',
-        }, 
-        tableHead: ['Descripción', 'Cantidad', 'Total'],
-        tableData: "",
+      facturaStruct: FacturaStruct,
+      facturaOptions: FacturaOptions,
+      facturaData: {
+        cedula: "",
+        cantidad: "",
+        monto: ""
+      },
+      facturaErrorMessage: "",
 
-        formDetalle:{
-          cantidad: '',
-          monto: '',
-          total:'',
-          subsidio:''
-        }, 
-          formTitulos:[],
-          loaded:true
-        };
-        
+      formRegistro: {
+        names: '',
+        lastnames: '',
+      },
+      tableHead: ['Descripción', 'Cantidad', 'Total'],
+      tableData: "",
+
+      formDetalle: {
+        cantidad: '',
+        monto: '',
+        total: '',
+        subsidio: ''
+      },
+      formTitulos: [],
+      loaded: true
+    };
+
+  }
+  async facturar() {
+
+  }
+  async buscarPersona(cantidad, monto) {
+    this.setState({
+      loaded: false, tableData: [
+        ['Gas', cantidad, Number(cantidad * 1.6).toFixed(2)],
+        ['Transporte', '1', Number(cantidad * monto - cantidad * 1.6).toFixed(2)],
+        ['', 'IVA', Number(cantidad * 1.6 * 0.12).toFixed(2)],
+        ['', 'Total', Number(cantidad * monto).toFixed(2)],
+        ['', 'Subsidio', Number(cantidad * 0.51122 * 15).toFixed(2)],],
     }
-    async facturar()
-    {
+    );
+    const { formRegistro, facturaData: { cedula } } = this.state;
+    const odoo = new Odoo({
+      host: 'scraping.fractalsoft.ec',
+      port: 8069, /* Defaults to 80 if not specified */
+      database: 'scraping',
+      username: 'ceduardodch@gmail.com', /* Optional if using a stored session_id */
+      password: 'CarlosDiaz2013', /* Optional if using a stored session_id */
+      protocol: 'http' /* Defaults to http if not specified */
+    })
 
+    await odoo.connect()
+      .then(response => { console.log(response); })
+      .catch(e => { console.log(e); })
+    /* Get partner from server example */
+    console.log(cedula);
+    const params = {
+      domain: [["identity", "=", cedula]],
+      fields: ["names", "lastnames"],
     }
-    async buscarPersona(cantidad, monto) {
-        this.setState({loaded:false, tableData: [
-          ['Gas', cantidad, Number(cantidad*1.6).toFixed(2)],        
-          ['Transporte', '1', Number(cantidad*monto - cantidad*1.6).toFixed(2) ],        
-          ['',  'IVA', Number(cantidad*1.6*0.12).toFixed(2)],        
-          ['',  'Total', Number(cantidad*monto).toFixed(2)],        
-          ['',  'Subsidio', Number(cantidad*0.51122*15).toFixed(2)],         ],} 
-        );
-        const { formRegistro, facturaData:{cedula}} = this.state;        
-        const odoo = new Odoo({
-          host: 'scraping.fractalsoft.ec',
-          port: 8069, /* Defaults to 80 if not specified */
-          database: 'scraping',
-          username: 'ceduardodch@gmail.com', /* Optional if using a stored session_id */
-          password: 'CarlosDiaz2013', /* Optional if using a stored session_id */
-          protocol: 'http' /* Defaults to http if not specified */
-        })
-    
-        await odoo.connect()
-          .then(response => {  console.log(response);})
-          .catch(e => { console.log(e); })
-        /* Get partner from server example */
-        console.log(cedula);
-        const params = {
-          domain: [["identity", "=", cedula]],
-          fields: [ "names", "lastnames"],
-        }
 
-        const context = {
-          domain: [["id", "=", 1]],
-        }
-    
-        await odoo.search_read('scraping.registro.civil', params, context)
-          .then(response => {
-            this.setState({
-                formRegistro:{
-              names: response.data[0].names,
-              lastnames: response.data[0].lastnames,            
-            }, 
-            loaded:true       
-            }
-            )        
-          })
-          .catch(e => { alert(e) });
-      
-
+    const context = {
+      domain: [["id", "=", 1]],
     }
-      
-      onChangeFormFactura = facturaValue => {
+
+    await odoo.search_read('scraping.registro.civil', params, context)
+      .then(response => {
         this.setState({
-          facturaData: facturaValue
-        });
-        console.log(facturaValue);
-      };
-    render(){
-        const {loaded,
-          facturaOptions,
-          facturaStruct,
-          facturaData,
-          tableHead,tableData } = this.state
-
-        if(!loaded)
-        {
-            return(<PreLoader/>);
-            }else{
-        return(
-        
-      <View style={styles.viewBody}>
-        <SafeAreaView style={styles.container}>
-      <ScrollView style={styles.scrollView}>
-          <Form
-            ref="facturaForm"
-            type={facturaStruct}
-            options={facturaOptions}
-            value={facturaData}
-            onChange={facturaValue => this.onChangeFormFactura(facturaValue)}
-          />
+          formRegistro: {
+            names: response.data[0].names,
+            lastnames: response.data[0].lastnames,
+          },
+          loaded: true
+        }
+        )
+      })
+      .catch(e => { alert(e) });
 
 
-      <Button title="Buscar" onPress={()=> this.buscarPersona(this.state.facturaData.cantidad, this.state.facturaData.monto)}></Button>
-      <Card title="DATOS BÁSICOS">
-      <Text style={styles.name}>Nombres: <Text style={styles.label}>{this.state.formRegistro.names}</Text></Text>
-      <Text style={styles.name}>Apellidos: <Text style={styles.label}>{this.state.formRegistro.lastnames}</Text></Text>
-     
-      </Card>
-      <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
-          <Row data={tableHead} style={styles.head} textStyle={styles.text}/>
-          <Rows data={tableData} textStyle={styles.text}/>
-        </Table>
+  }
 
-        <Button title="Facturar" onPress={()=> this.facturar()}></Button>
-        </ScrollView>
-    </SafeAreaView>
-</View>
-    );}}
+  onChangeFormFactura = facturaValue => {
+    this.setState({
+      facturaData: facturaValue
+    });
+    console.log(facturaValue);
+  };
+  register_user = () => {
+    const {
+      formRegistro,
+      facturaData
+    } = this.state
+    var user_name = formRegistro.names;
+    var user_lastname = formRegistro.lastnames;
+    var user_monto= facturaData.monto;
+    var user_cedula= facturaData.cedula;
+    db.transaction(function (tx) {
+      tx.executeSql(
+        'INSERT INTO table_user (user_name, user_lastname) VALUES (?,?)',
+        [user_name, user_lastname],
+        (tx, results) => {
+          console.log('Results', results.rowsAffected);
+          if (results.rowsAffected > 0) {
+            Alert.alert(
+              'Success',
+              'You are Registered Successfully',
+              [
+                {
+                  text: 'Ok',
+                  onPress: () =>
+                    console.log("usususus"),
+                },
+              ],
+              { cancelable: false }
+            );
+          } else {
+            alert('Registration Failed');
+          }
+        }
+      );
+    });
+
+  }
+  render() {
+    const { loaded,
+      facturaOptions,
+      facturaStruct,
+      facturaData,
+      tableHead, tableData } = this.state
+
+    if (!loaded) {
+      return (<PreLoader />);
+    } else {
+      return (
+
+        <View style={styles.viewBody}>
+          <SafeAreaView style={styles.container}>
+            <ScrollView style={styles.scrollView}>
+              <Form
+                ref="facturaForm"
+                type={facturaStruct}
+                options={facturaOptions}
+                value={facturaData}
+                onChange={facturaValue => this.onChangeFormFactura(facturaValue)}
+              />
+
+
+              <Button title="Buscar" onPress={() => this.buscarPersona(this.state.facturaData.cantidad, this.state.facturaData.monto)}></Button>
+              <Card title="DATOS BÁSICOS">
+                <Text style={styles.name}>Nombres: <Text style={styles.label}>{this.state.formRegistro.names}</Text></Text>
+                <Text style={styles.name}>Apellidos: <Text style={styles.label}>{this.state.formRegistro.lastnames}</Text></Text>
+
+              </Card>
+              <Table borderStyle={{ borderWidth: 2, borderColor: '#c8e1ff' }}>
+                <Row data={tableHead} style={styles.head} textStyle={styles.text} />
+                <Rows data={tableData} textStyle={styles.text} />
+              </Table>
+
+              <Button title="Facturar" onPress={() => this.facturar()}></Button>
+              <Button title="Probar" onPress={() => this._guardarSqlLite()}></Button>
+            </ScrollView>
+          </SafeAreaView>
+        </View>
+      );
+    }
+  }
 }
 
 const styles = StyleSheet.create({
-    viewBody:{
-        flex:1,        
-        backgroundColor:"#fff",
-        marginBottom:20,
-        marginLeft:25,
-        marginTop:15,
-        marginRight:25
-    },
-    inputBox: {
-        width: 300,
-        backgroundColor: 'rgba(255, 255,255,0.2)',
-        borderRadius: 25,
-        paddingHorizontal: 16,
-        fontSize: 16,
-        color: '#ffffff',
-        marginVertical: 10,
-        marginLeft:15,
-        marginTop:15,
-        marginRight:15
-      },
-    
-      button: {
-        width: 300,
-        backgroundColor: '#1c313a',
-        borderRadius: 25,
-        marginVertical: 10,
-        paddingVertical: 13,
-        marginLeft:15,
-        marginTop:15,
-        marginRight:15
-      },
-      name:{
-        fontWeight: "bold"
-      },
-      label:{
-        fontWeight: "normal",
-        fontSize:14
-      },
-      head: { height: 40, backgroundColor: '#f1f8ff' },
-      text: { margin: 6 }
+  viewBody: {
+    flex: 1,
+    backgroundColor: "#fff",
+    marginBottom: 20,
+    marginLeft: 25,
+    marginTop: 15,
+    marginRight: 25
+  },
+  inputBox: {
+    width: 300,
+    backgroundColor: 'rgba(255, 255,255,0.2)',
+    borderRadius: 25,
+    paddingHorizontal: 16,
+    fontSize: 16,
+    color: '#ffffff',
+    marginVertical: 10,
+    marginLeft: 15,
+    marginTop: 15,
+    marginRight: 15
+  },
+
+  button: {
+    width: 300,
+    backgroundColor: '#1c313a',
+    borderRadius: 25,
+    marginVertical: 10,
+    paddingVertical: 13,
+    marginLeft: 15,
+    marginTop: 15,
+    marginRight: 15
+  },
+  name: {
+    fontWeight: "bold"
+  },
+  label: {
+    fontWeight: "normal",
+    fontSize: 14
+  },
+  head: { height: 40, backgroundColor: '#f1f8ff' },
+  text: { margin: 6 }
 })
--- app/screens/Map.js
+++ app/screens/Map.js
@@ -1,20 +1,76 @@
-import React, {Component} from "react";
-import {StyleSheet,View, Text} from  "react-native"
+import React, { Component } from "react";
+import { StyleSheet, View, FlatList, Text,Alert } from "react-native"
+import { Icon } from "react-native-elements";
+import * as SQLite from 'expo-sqlite';
+const db = SQLite.openDatabase("Factura.db");
 
-export default class Map extends Component
-{
-    render(){return(
-<View style="styles.viewBody">
-    <Text>Map Screen</Text>
-</View>
-    );}
+export default class Map extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            FlatListItems: [],
+        };
+        this.view_user();
+    }
+    view_user = () => {
+        db.transaction(tx => {
+            tx.executeSql('SELECT * FROM table_user', [], (tx, results) => {
+                var temp = [];
+                for (let i = 0; i < results.rows.length; ++i) {
+                    temp.push(results.rows.item(i));
+                }
+                this.setState({
+                    FlatListItems: temp,
+                });
+            });
+        });
+    }
+    ListViewItemSeparator = () => {
+        return (
+            <View style={{ height: 0.2, width: '100%', backgroundColor: '#808080' }} />
+        );
+    };
+    render() {
+        return (
+            <View style="styles.viewBody">
+                <Icon
+                    type="material-community"
+                    name="cached"
+                    size={50}
+                    containerStyle={styles.containerIconClose}
+                    onPress={() => this.view_user()}
+                />
+                <FlatList
+                    data={this.state.FlatListItems}
+                    ItemSeparatorComponent={this.ListViewItemSeparator}
+                    keyExtractor={(item, index) => index.toString()}
+                    renderItem={({ item }) => (
+                        <View key={item.user_id} style={{ backgroundColor: 'white', padding: 20 }}>
+                            <Text>Id: {item.user_id}</Text>
+                            <Text>Name: {item.user_name}</Text>
+                            <Text>LastName: {item.user_lastname}</Text>
+                        </View>
+                    )}
+                />
+            </View>
+        );
+    }
 }
 
+
 const styles = StyleSheet.create({
-    viewBody:{
-        flex:1,
-        alignItems:"center",
-        justifyContent:"center",
-        backgroundColor:"#fff"
-    }
+    viewBody: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#fff"
+    },
+    containerIconClose: {
+        position: "absolute",
+        bottom: 13,
+        right: 13,
+        backgroundColor: "#fff",
+        borderRadius:25
+      },
+    
 })
