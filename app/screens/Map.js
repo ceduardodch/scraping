e6bde27d9960b15191afdc81eb0418123d066a4c
Carlos Diaz <carlos.diaz@fractalsoft.ec>
2020-02-03 13:45:21 -0500

    date

--- app/screens/Map.js
+++ app/screens/Map.js
@@ -230,11 +230,13 @@ export default class Map extends Component {
         const dataFact ={
                           partner_id: partner_id,
                           type: "out_invoice",
+                          date_invoice: Date().getDate(),
                           total: user_total,
                           montoiva: user_iva,
                           baseimpgrav: user_cantidad * 1.6,
                           baseimponible: user_transporte,
                           subtotal: user_subtotal - user_iva,
+
                         }
         await odoo
           .create(
